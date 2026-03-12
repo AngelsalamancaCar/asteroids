216cf34fbfaec2026cb62341c72900cfae4d4d6f
AngelSalamancaCar <227869586+AngelsalamancaCar@users.noreply.github.com>
2026-03-12 10:27:21 -0600

    avances a ch3 l4

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 # ------ Import dependencies --------
 import pygame
+from pygame.time import Clock
+
 from constants import *
 from logger import log_state
+from player import Player
+
+# ------- Start game config ---------
 
 
 def main():
@@ -15,6 +20,15 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # Create clock object
+    clock = pygame.time.Clock()
+
+    # Create delta time variable
+    dt = 0
+
+    # Instantiate player
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     # Initiate the infinite loop
     while True:
         # call the logger
@@ -24,11 +38,20 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        # -- update all objects --
+        player.update(dt)
+
         # --- fill screen with black ----
         screen.fill("black")
 
+        # Draw player
+        player.draw(screen)
+
         # Refresh the screen
         pygame.display.flip()
+
+        # End of loop timing
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
